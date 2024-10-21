caef30e3e8177cf5d5b45651a5c69b15ddd72fe4
Rajat <rishichoudhary08102002@gmail.com>
2024-10-21 22:17:44 +0530

    Updated the hooks to perform much better along with reduced props

--- src/Components/ShowDetails.js
+++ src/Components/ShowDetails.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
 import Details from './Details';
 import './ShowDetails.css';
-export default function ShowDetails({ visibility, start, end, parseISO8601Duration, formatDuration, data, updatetime }) {
+export default function ShowDetails({ data }) {
     const [isvisible, setisvisible] = useState(false);
     const [text, settext] = useState("show details");
-    if (visibility === false) return (<></>);
     const handleDetails = (e) => {
         setisvisible(prev => !prev)
         settext(prev => {
@@ -19,7 +18,7 @@ export default function ShowDetails({ visibility, start, end, parseISO8601Durati
                 <a href="/" className="App-link" onClick={handleDetails}>{text}</a>
             </div>
             <div className="Details" >
-                <Details items={data.slice((start > 0) ? (start - 1) : 0, ((end === Infinity) ? (end) : (end - 1)) + 1)} isvisible={isvisible} parseISO8601Duration={parseISO8601Duration} formatDuration={formatDuration} start={start === -1 ? 1 : start} updatetime={updatetime}></Details>
+                <Details items={data} isvisible={isvisible}></Details>
             </div>
         </>
     )
